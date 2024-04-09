0b3bcc278df1c07e9ac7c0029313f14ff9de2e92
Ping Gong <gpzlx1@mail.ustc.edu.cn>
2024-04-09 12:46:51 +0800

    remove unused  code

--- python/shmtensor/disk_tensor.py
+++ python/shmtensor/disk_tensor.py
@@ -45,19 +45,3 @@ class DiskTensor:
     @property
     def size(self):
         return self.tensor_.size
-
-
-if __name__ == "__main__":
-    import numpy as np
-    npy_data = np.random.randn(2, 3, 4).astype(np.float64)
-    npy_tensor = torch.from_numpy(npy_data)
-
-    # save
-    np.save("a.npy", npy_data)
-
-    tensor = DiskTensor("a.npy")
-
-    assert torch.equal(tensor.tensor_, npy_tensor)
-
-    print(tensor.tensor_)
-    print(npy_tensor)
